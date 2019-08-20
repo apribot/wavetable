26e47d3f0de46d1314fe8153d3c1d463e7db63af
mint <apribot@gmail.com>
2019-08-19 23:57:40 -0400

    wavetable blending

--- go
+++ go
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcc -o test test.c && ./test | gnuplot -p -e 'plot "/dev/stdin"'
+gcc -o test test.c && ./test | gnuplot -p -e 'set terminal png size 400,300; set output "xyz.png"; plot "/dev/stdin"'
--- test.c
+++ test.c
@@ -5,13 +5,38 @@
 #include <stdio.h>
 #include "wavetable.h"
 
+int * getWave(int pos) {
+	static int form[256];
+	int position;
+	int remainder;
+
+	position = pos / 28;
+
+	remainder = pos % 28;
+	// interpolate position
+	// 1008 positions (36 is a factor)
+	// arduino returns 0 - 1023 via analogRead()
+	// trim off top/bottom?
+	if (remainder == 0)
+	{
+		for (int i = 0; i < 256; ++i) {
+			form[i] = wavetable[position][i];
+		}
+	} else {
+		for (int i = 0; i < 256; ++i) {
+			form[i] = ((wavetable[position+1][i] * remainder) + (wavetable[position][i] * (28 - remainder))) / 28;
+		}
+	}
+	return form;
+}
+
 int main()
 {
-	int bit;
-
-   	for(int t = 0; t < 256; t++) {
-  		bit = (TRIANGLE0[t] + SAW11[t]) / 2;
-  		printf("%d \n", bit);
+	int *bit;
+	bit = getWave(999);
+   	
+   	for(int t = 0; t < 255; t++) {
+  		printf("%d \n", *(bit + t));
 	}
    return 0;
 }
